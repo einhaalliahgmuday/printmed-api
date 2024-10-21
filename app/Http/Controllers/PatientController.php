0970fa0550911ce6f0bbf0d2dbaf7037273585dd
Einha Muday <132117926+einhaalliahgmuday@users.noreply.github.com>
2024-10-21 20:16:33 +0800

    update on patient

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -2,135 +2,68 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\ConsultationRecord;
 use App\Models\Patient;
 use App\Models\Payment;
-use App\Models\PhysicianPatient;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class PatientController extends Controller
 {
-    use CommonMethodsTrait;
-
     public function index(Request $request)
     {        
         $user = $request->user();
 
         $request->validate([
             'search' => 'string',
-            'sex' => 'string'
+            'sex' => 'string',
+            'sort_by' => 'string|in:last_name,patient_number,follow_up_date',
+            'sort_direction' => 'string|in:asc,desc'
         ]);
 
-        //only physician and secretary can only access patient records
-        if (in_array($user->role, ['physician', 'secretary'])) 
-        {
-            //if role is physician, it will only query the patients of it; else, all
-            $query = $user->role === 'physician' ? $user->patients() : Patient::query();
-
-            if ($query != null)
-            {
-                //narrows the query with search
-                if($request->filled('search') )
-                {
-                    $search = $request->search;
-
-                    $query->where(function($q) use ($search)
-                    {
-                        $q->where('full_name', 'LIKE', "%$search%")
-                        ->orWhere('patient_id', 'LIKE', "%$search%")
-                        ->orWhere('birthday', 'LIKE', "%$search%");
-                    });
-                }
-
-                //narrows the query with sex
-                if($request->filled('sex'))
-                {
-                    $query->where('sex', $request->sex);
-                }
-
-                //sort the query by either last_name, patient_id, or birthday
-                if($request->filled('sort_by') && in_array($request->sort_by, ['last_name', 'patient_id', 'birthdate', 'follow_up_date'])) 
-                {
-                    $direction = $request->input('sort_direction', 'asc');
-
-                    $query->orderBy($request->input('sort_by'), $direction);
-                } 
-                else {
-                    $query->orderBy('updated_at');
-                }
-
-                $patients = $query->select('patients.id', 'patients.patient_id', 'patients.full_name', 'patients.suffix', 'patients.birthday', 'patients.sex', 'patients.last_visit')->paginate(15);
-                $patients->appends($request->all()); //appends the request parameters to paginate URLs
-
-                return $patients;
-            }
-        }
-
-        return response()->json([
-            'success' => false,
-            'message' => 'Unauthorized request.'
-        ], 401);
-    }
-
-    public function isPatientExists(Request $request) 
-    {
-        $request->validate([
-            'first_name' => 'required|string|max:100',
-            'last_name'=> 'required|string|max:100',
-            'birthday' => 'date',
-            'sex' => 'string|max:20'
-        ]);
-
-        $patients = Patient::select('id', 'patient_id', 'full_name', 'suffix', 'birthday', 'sex', 'last_visit')
-                            ->where('first_name', 'LIKE', "%$request->first_name%")
-                            ->where('last_name', $request->last_name)
-                            ->where('birthday', $request->birthday)
-                            ->where('sex', $request->sex)
-                            ->get();
-
-        if ($patients->count() <= 0) 
-        {
-            return response()->json([
-                'exists' => false
-            ]);
-        }
-
-        return $patients;
-    }
-
-    public function getPatientsCount(Request $request)
-    {
-        $request->validate([
-            'department' => 'string|max:100',
-            'date_from' => 'date',
-            'date_until' => 'date|after_or_equal:date_from',
-        ]);
-
-        //uses Payment model to get the number of patients visited
-        $query = Payment::query();
-
-        if($request->filled('department'))
-        {
-            $query->where('department', $request->department);
-        }
-
-        if($request->filled('date_from'))
-        {
-            $query->where('date', '>=', $request->date_from);
-        }
-
-        if($request->filled('date_until'))
-        {
-            $query->where('date', '<=', $request->date_until);
-        }
-
-        $query->distinct('patient_id');
-        $patientsCount = $query->count();
         
-        return response()->json([
-            'patients_count' => $patientsCount
-        ]);
+        //if role is physician, it will only query the patients of the physician
+        $query = $user->role == 'physician' ? $user->patients() : Patient::query();
+
+        //query can be filter based on search (name, patient_number), sex
+        //it can also be sorted by last_name, patient_number, and follow_up_date
+        if ($query != null)
+        {
+            if($request->filled('search') )
+            {
+                $search = $request->search;
+
+                $query->where(function($q) use ($search)
+                {
+                    $q->where('first_name', 'LIKE', "%$search%")
+                    ->orWhere('middle_name', 'LIKE', "%$search%")
+                    ->orWhere('last_name', 'LIKE', "%$search%")
+                    ->orWhere('suffix', 'LIKE', "%$search%")
+                    ->orWhere('patient_number', 'LIKE', "%$search%");
+                });
+            }
+
+            if($request->filled('sex'))
+            {
+                $query->where('sex', $request->sex);
+            }
+
+            if($request->filled('sort_by') && in_array($request->sort_by, ['last_name', 'patient_number', 'follow_up_date'])) 
+            {
+                $direction = $request->input('sort_direction', 'asc');
+
+                $query->orderBy($request->input('sort_by'), $direction);
+            } 
+            else {
+                $query->orderBy('updated_at');
+            }
+
+            $patients = $query->paginate(15);
+            $patients->appends($request->all()); //appends the request parameters to pagination URLs
+
+            return $patients;
+        }
+
+        return response()->json(['patients' => null]);
     }
 
     public function store(Request $request)
@@ -140,7 +73,7 @@ class PatientController extends Controller
             'middle_name' => 'string|max:100',
             'last_name'=> 'required|string|max:100',
             'suffix' => 'string|max:20',
-            'birthday' => 'date',
+            'birthdate' => 'date',
             'sex' => 'string|max:20',
             'address' => 'string|max:255',
             'civil_status' => 'string|max:20',
@@ -148,52 +81,37 @@ class PatientController extends Controller
             'phone_number' => 'string|max:30'
         ]);
 
-        $fields['full_name'] = $this->getFullName($request);
-        $fields['last_visit'] = Carbon::now('Asia/Manila')->toDateString();
-        $fields['patient_id'] = Patient::generatePatientNumber();
+        $fields['patient_number'] = Patient::generatePatientNumber();
 
         $patient = Patient::create($fields);
 
         return $patient;
     }
 
-    public function show(Request $request, Patient $patient)
+    public function show(Patient $patient)
     {
-        $user = $request->user();
-
-        $consultationRecords = ConsultationRecord::select('id', 'chief_complaint', 'primary_diagnosis', 'created_at')
-                                                ->where('patient_id', $patient->id);
-
-        //to return consultation records by a specific physician only
-        if ($user->role === 'physician') 
-        {
-            $consultationRecords->where('physician_id', $user->id);
-        }
-
-        $consultationRecords = $consultationRecords->get();
+        $consultationRecords = $patient->consultationRecords()->paginate(10);
 
         return response()->json([
             'patient' => $patient,
-            'consultation_records' => $consultationRecords
+            'consultationRecords' => $consultationRecords
         ]);
     }
 
     public function update(Request $request, Patient $patient)
     {
         $fields = $request->validate([
-            'first_name' => 'required|string|max:100',
+            'first_name' => 'string|max:100',
             'middle_name' => 'string|max:100',
-            'last_name'=> 'required|string|max:100',
+            'last_name'=> 'string|max:100',
             'suffix' => 'string|max:20',
-            'birthday' => 'date',
+            'birthdate' => 'date',
             'sex' => 'string|max:20',
             'address' => 'string|max:255',
             'civil_status' => 'string|max:20',
             'religion' => 'string|max:255',
             'phone_number' => 'string|max:30'
         ]);
-
-        $fields['full_name'] = $this->getFullName($request);
 
         $patient->update($fields);
 
@@ -202,10 +120,15 @@ class PatientController extends Controller
 
     public function destroy(Patient $patient)
     {
-        $dateThreshold = Carbon::now()->subYears(10)->toDateString();
+        $dateThreshold = Carbon::now()->subYears(10);
+        $lastConsultationRecordDate = $patient->consultationRecords()
+                                            ->select('updated_at')
+                                            ->orderBy('updated_at','desc')
+                                            ->pluck('updated_at')
+                                            ->first();
 
-        //if patient's last consultation date is not past 10 years, patient cannot be deleted
-        if ($patient->last_visit >= $dateThreshold) 
+        //if patient or patient's last consultation date is not past 10 years, patient cannot be deleted
+        if ($patient->updated_at >= $dateThreshold || $lastConsultationRecordDate >= $dateThreshold) 
         {
             return response()->json([
                 'success' => false,
@@ -219,5 +142,58 @@ class PatientController extends Controller
             'success' => true,
             'message' => 'Patient successfully deleted.'
         ], 200);
+    }
+
+    public function getDuplicatePatients(Request $request) 
+    {
+        $request->validate([
+            'first_name' => 'required|string|max:100',
+            'last_name'=> 'required|string|max:100',
+            'birthdate' => 'required|date',
+            'sex' => 'required|string|max:6'
+        ]);
+
+        $patients = Patient::where('first_name', 'LIKE', "%$request->first_name%")
+                            ->where('last_name', $request->last_name)
+                            ->where('birthdate', $request->birthdate)
+                            ->where('sex', $request->sex)
+                            ->get();
+
+        return $patients;
+    }
+
+    public function getPatientsCount(Request $request)
+    {
+        $request->validate([
+            'department_id' => 'integer',
+            'date_from' => 'date',
+            'date_until' => 'date|after_or_equal:date_from',
+        ]);
+
+        //uses Payment model to get the number of patients visited
+        $query = Payment::query();
+
+        if($request->filled('department_id'))
+        {
+            $query->where('department_id', $request->department);
+        }
+
+        if($request->filled('date_from'))
+        {
+            $query->where('date', '>=', $request->date_from);
+        }
+
+        if($request->filled('date_until'))
+        {
+            $query->where('date', '<=', $request->date_until);
+        }
+
+        $query->distinct('patient_id');
+
+        $patientsCount = $query->count();
+        
+        return response()->json([
+            'patients_count' => $patientsCount
+        ]);
     }
 }
--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -2,16 +2,16 @@
 
 namespace App\Models;
 
+use App\Traits\CommonMethodsTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Patient extends Model
 {
-    use HasFactory;
+    use HasFactory, CommonMethodsTrait;
 
     protected $fillable = [
-        'patient_id',
-        'full_name',
+        'patient_number',
         'first_name',
         'middle_name',
         'last_name',
@@ -21,27 +21,55 @@ class Patient extends Model
         'address',
         'civil_status',
         'religion',
-        'phone_number',
-        'last_visit',
-        'follow_up_date'
+        'phone_number'
     ];
 
     protected $hidden = [
         'created_at',
-        'updated_at'
+        'updated_at',
+        'consultation_records',
+        'physicians'
+    ];
+
+    protected $appends = [
+        'full_name',
+        'last_visit',
+        'follow_up_date'
     ];
 
     public static function generatePatientNumber()
     {
         $year = date('Y');
-        $lastPatient = self::select('patient_id')
-                        ->where('patient_id', 'like', "%$year-%")
-                        ->orderBy('patient_id', 'desc')
+        $lastPatient = self::select('patient_number')
+                        ->where('patient_number', 'like', "%$year-%")
+                        ->orderBy('patient_number', 'desc')
                         ->first();
         
         $increment = $lastPatient ? (int) substr($lastPatient->patient_id,5) + 1 : 1;
 
         return sprintf('%s-%05d', $year, $increment);
+    }
+
+    public function getFullNameAttribute()
+    {
+        return $this->getFullName($this->first_name, $this->middle_name, $this->last_name, $this->suffix);
+    }
+
+    public function getLastVisitAttribute()
+    {
+        $latestConsultationRecord = $this->consultationRecords()->where('patient_id', $this->id)->latest('updated_at')->first();
+        
+        return  $latestConsultationRecord ? $latestConsultationRecord->updated_at->toDateString() : $this->created_at->toDateString();
+    }
+
+    public function getFollowUpDateAttribute()
+    {
+        return $this->consultationRecords()->where('patient_id', $this->id)->latest('updated_at')->first() ?-> follow_up_date;
+    }
+
+    public function consultationRecords()
+    {
+        return $this->hasMany(ConsultationRecord::class, 'patient_id');
     }
 
     public function physicians()
